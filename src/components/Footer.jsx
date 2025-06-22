cf41976ae522541577e958f73f3f536b33a3fb03
Nelson Donoso <nelsondonoso@gmail.com>
2025-06-21 22:05:36 -0400

    Hito 1 - React Introduccion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="pie">
-        <h5 className="pie">@2025 - Pizeria Mamma Mia! - Todos los derehos reservados</h5>
+        <h5 className="pie">@2025 - Pizzeria Mamma Mia! - Todos los derehos reservados</h5>
     </div>
   )
 }
